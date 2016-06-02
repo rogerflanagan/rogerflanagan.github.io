d24afc419a7a547e18eadf18a07cfd8d60fd3c82
rogerf@zoosk.com <rogerf@zoosk.com>
2016-06-02 14:49:57 -0700

    Testing out jquery waypoint navigation

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         hljs.highlightBlock(block);
     });
 
-    // Navigation
+    // Sub Navigation
     $('.eros-nav__title').click(function() {
         event.stopPropagation();
         $('.js-menu-toggle').toggle();
@@ -15,4 +15,59 @@ $(document).ready(function() {
     $(document).click(function() {
         $('.js-menu-toggle').hide();
     });
+
+    // Page Navigation
+    // ======================================
+    // Helper functions
+    // ======================================
+    // Get section or article by href
+    function getRelatedContent(el){
+      return $($(el).attr('href'));
+    }
+    // Get link by section or article id
+    function getRelatedNavigation(el){
+      return $('.js-nav-list a[href=#'+$(el).attr('id')+']');
+    }
+
+
+    // ======================================
+    // Smooth scroll to content
+    // ======================================
+    $('.js-nav-list a').on('click',function(e){
+      e.preventDefault();
+      $('html,body').animate({scrollTop:getRelatedContent(this).offset().top - 20})
+    });
+
+
+    // ======================================
+    // Waypoints
+    // ======================================
+    // Default cwaypoint settings
+    // - just showing
+    var wpDefaults={
+      context: window,
+      continuous: true,
+      enabled: true,
+      horizontal: false,
+      offset: 0,
+      triggerOnce: false
+    };
+
+    $('.eros-example')
+       .waypoint(function(direction) {
+         // Highlight element when related content
+         // is 10% percent from the bottom...
+         // remove if below
+         getRelatedNavigation(this).toggleClass('eros-nav__link-active', direction === 'down');
+       }, {
+         offset: '90%' //
+       })
+       .waypoint(function(direction) {
+         // Highlight element when bottom of related content
+         // is 100px from the top - remove if less
+         // TODO - make function for this
+         getRelatedNavigation(this).toggleClass('eros-nav__link-active', direction === 'up');
+       }, {
+         offset: function() {  return -$(this).height() + 100; }
+       });
 });
